9c428669705e7389252d2be040038a847080825c
tylermeyers351 <tylermeyers351@gmail.com>
2023-10-10 18:00:30 -0700

    Firebase functionality

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const notesListingInDB = ref(database, "notesListing")
 
-onValue(notesListingInDB, function(snapshot) {
-    const data = snapshot.val()
-    console.log(data[0])
-})
-
 // Elements to store.
 const categoryEl = document.querySelector('#inputState')
 const titleEl = document.querySelector('#formTitle')
@@ -30,10 +25,38 @@ const dateObject = {
     day: d.getDate()
 }
 
+onValue(notesListingInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let notesArray = Object.values(snapshot.val())
+
+        listingEl.innerHTML = ""
+
+        for (let i = notesArray.length - 1; i >= 0; i --) {
+            let note = notesArray[i]
+            
+            let category = note.category
+            let title = note.title
+            let content = note.content
+
+            let month = note.date.month
+            let year = note.date.year
+            let day = note.date.day
+
+            // Updates the <ul> with all of list items from the database.
+            updateListItem(title, category, content, month, day, year)
+        }
+    } else {
+        listingEl.innerHTML = "No items"
+    }
+})
+
 // Add an event listener to the submit button and run functions when submitted.
 submitButtonEl.addEventListener('click', function() {
-    updateServer(titleEl.value, categoryEl.value, contentEl.value) 
-    updateListing(titleEl.value, categoryEl.value, contentEl.value)
+    
+    // Updates the Firebase server with all of the form items that were submitted.
+    updateServer(titleEl.value, categoryEl.value, contentEl.value)
+
+    resetValues()
 })
 
 // Function that updates the server with a new listing object (from the form values).
@@ -49,7 +72,7 @@ function updateServer(titleValue, categoryValue, contentValue) {
 }
 
 // Function that updates the listing with the newly added listing object (and clears form values).
-function updateListing(titleValue, categoryValue, contentValue) {
+function updateListItem(titleValue, categoryValue, contentValue, month, day, year) {
 
     let newEl = document.createElement('li')
     newEl.classList.add('border', 'rounded', 'p-3', 'mb-2')
@@ -62,7 +85,7 @@ function updateListing(titleValue, categoryValue, contentValue) {
     // Update date.
     const dateP = document.createElement('p')
     dateP.classList.add('small')
-    dateP.textContent =  dateObject.month + "/" + dateObject.day + "/" + dateObject.year
+    dateP.textContent =  month + "/" + day + "/" + year
 
     // Update content.
     const contentP = document.createElement('p')
@@ -77,7 +100,11 @@ function updateListing(titleValue, categoryValue, contentValue) {
     // Append <ul> tag.
     listingEl.append(newEl)
 
-    // Clear values.
+    
+}
+
+// Clears form values.
+function resetValues() {
     categoryEl.selectedIndex = 0
     titleEl.value = ""
     contentEl.value = ""
